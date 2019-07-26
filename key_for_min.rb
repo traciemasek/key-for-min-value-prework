7e56310d18dc31f5b4677a76f104813a284f3302
Tracie Masek <traciemasek@gmail.com>
2019-07-26 18:48:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
     return nil 
   else
     smallest = 0 
+    small_key = nil 
     name_hash.each do |key, value|
       if value <= smallest
         smallest = value
